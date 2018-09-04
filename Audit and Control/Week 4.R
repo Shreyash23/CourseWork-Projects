10ffed745e88454db8945724c53fe30e31c19598
Shreyash23 <shreyash.shukla.2304.com>
2018-09-04 03:45:09 -0500

    Commit

--- Audit and Control/Week 4.R
+++
@@ -1,9 +0,0 @@
-inv <-  defData(varname="Inventory",dist="nonrandom",formula = 7,id="idnum")
-
-inv <-defData(inv,varname="invoice" ,dist="uniform",formula = "1;1000")
-
-inv <- defData(inv,varname="sku",dist = "categorical",formula = "10000;11000")
-
-inv <- defData(inv,varname = "quantity",dist = "poisson",formula = "Inventory -1")
-
-InventoryFile <- genData(1000,inv)
